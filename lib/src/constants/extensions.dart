9978b958fe8f9ff01dc0dc0089dc6cebb2ffa228
may.wang <may.wang@designlibro.com>
2024-08-28 18:01:42 +0800

    修改代码保存问题

--- lib/src/constants/extensions.dart
+++ lib/src/constants/extensions.dart
@@ -7,11 +7,9 @@ import 'package:dio/dio.dart' show Response;
 import 'constants.dart';
 
 extension ResponseExtension on Response<dynamic> {
-  int get startTimeMilliseconds =>
-      requestOptions.extra[DIO_EXTRA_START_TIME] as int;
+  int get startTimeMilliseconds => DateTime.now().millisecondsSinceEpoch;
 
-  int get endTimeMilliseconds =>
-      requestOptions.extra[DIO_EXTRA_END_TIME] as int;
+  int get endTimeMilliseconds => DateTime.now().millisecondsSinceEpoch;
 
   DateTime get startTime =>
       DateTime.fromMillisecondsSinceEpoch(startTimeMilliseconds);
